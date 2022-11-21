3f79f7a5483b32d5609ca8a104a54b05936c6c4d
uguravan <114769074+uguravan@users.noreply.github.com>
2022-11-21 04:14:09 +0300

    FlightFare

--- src/UgurJava/Denemeler/dene1.java
+++ src/UgurJava/Denemeler/dene1.java
@@ -1,16 +1,34 @@
 package UgurJava.Denemeler;
 
-public class dene1 {
-    String name= "Oguzhan";
-    int yaş= 46;
-    boolean flag= false;
-    char karak= 'A';
 
-    byte a;
-    short b;
-    int c;
-    long l;
-     float f;
-     double d;
+import java.util.Scanner;
+
+public class dene1 {
+       public static void main(String[] args) {
+        /*Scanner scan = new Scanner(System.in);
+        System.out.print("lütfen password giriniz: ");
+        String pass= scan.nextLine();
+        System.out.println(pass);
+        System.out.println();//dummy password
+        System.out.println("merhabalar");*/
+        double age= 50.9;
+        int a=5;
+        int b=4;
+
+        yaşbulan((int) age);
+        boolean flag= false;
+
+        if(flag){
+            System.out.println("flag = true");
+        }else System.out.println("flag falsemuş");
+
+        System.out.println(flag ? "ok": "not ok");
+        String name = a==b ? "Ahmet" : (a<b ? "Mehmet" : (a>10 ? "Kevser" : "Oğuzhan"));
+        System.out.println(name);
+    }
+    public static void yaşbulan(int age){
+        age+=5;
+        System.out.println(age);
+    }
 
 }
---
+++ src/UgurJava/Homeworks/FlightFare.java
@@ -0,0 +1,57 @@
+package UgurJava.Homeworks;
+    /*
+    ----------------------------------------  ÖDEV  ----------------------------------------
+    * A şehrinden uçmak isteyen bir kişi B şehrine 500 KM, C şehrine 700 KM, D şehrine 900 KM mesafededir.
+    * KM Birim Fiyatı: 0.10$
+    * 12 yaşından küçükse toplam fiyata %50 indirim uygulanacaktır.
+    * 12 - 24 yaş arasındaysa %10 indirim uygulanacaktır.
+    * 65 yaşından büyükse %30 indirim uygulanacaktır.
+    * Gidiş - Dönüş bilet alınırsa yaş indirimlerinden hariç %20 indirim uygulanacaktır.
+    * Bu koşullara göre yolcudan gideceği mesafeyi isteyip, uçak bilet ücretini hesaplayan kodu yazınız.
+    */
+import java.util.Scanner;
+
+public class FlightFare {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Flight fare : " + finalPrice(age(city())));
+        scan.close();
+    }
+
+    public static double city() {
+        System.out.print("Please choose your destination: B or C or D ");
+        char city = scan.next().trim().toUpperCase().charAt(0);
+        double cityBill=0;
+        switch (city) {
+            case 'B': cityBill=500*0.1; break;
+            case 'C': cityBill=700*0.1; break;
+            case 'D': cityBill=900*0.1; break;
+            default: System.out.println("Please enter your information appropriately."); city();
+        }
+        return cityBill;
+    }
+
+    public static double age(double cityBill) {
+        double ageBill=0;
+        System.out.print("Please enter your age: ");
+        int age = scan.nextInt();
+        if (age>=0) {
+            ageBill = age > 65 ? cityBill * 0.7 : age > 24 ? cityBill : age > 12 ? cityBill * 0.9 : cityBill * 0.5;
+        } else {System.out.println("Please enter your information appropriately."); age(cityBill);}
+        return ageBill;
+    }
+
+    public static double finalPrice(double ageBill) {
+        double finalP=0;
+        System.out.print("Please choose your flight type: Round-trip<1> or One-way<2> ");
+        int choice = scan.nextInt();
+
+        switch (choice) {
+            case 1: finalP = 2*ageBill*0.8; break;
+            case 2: finalP = ageBill; break;
+            default: System.out.println("Please enter your information appropriately."); finalPrice(ageBill);
+        }
+        return finalP;
+    }
+}
